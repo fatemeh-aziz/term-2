d9edd1753cfc07c29bade728c085c31a558ace32
aLmA <fatemehazizi>
2023-08-07 13:44:39 +0330

    com

--- week 3/first-project/src/component/ My table.tsx
+++ week 3/first-project/src/component/ My table.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 
 interface product {
     ID:number,
@@ -11,17 +12,31 @@ interface prop{
 
 
 export default function MyTable({mydata}:prop) {
-    console.log(mydata)
+
+ const [mydataState,setmydataState]=useState(mydata);
+
+ const deleteHandler=(id:number) =>{
+    let result=mydataState.filter((item)=>{
+        return item.ID !== id
+    });
+    setmydataState(result);
+ }
     return (
-        <table className={"w-full bg-white"}>
+        <table className={"w-full bg-white text-black text-center"}>
+            {
+                caption && <caption style={{captionSide: "bottom"}}>{caption}</caption>
+            }
             <tbody>
             {
                 mydata.map((item:product,index:number)=> {
                     return(
                     <tr>
-                        <td>{mydata.ID}</td>
-                        <td>{mydata.name}</td>
-                        <td>{mydata.price}</td>
+                        <td className={"border"}>{mydata.ID}</td>
+                        <td className={"border"}>{mydata.name}</td>
+                        <td className={"border"}>{mydata.price}</td>
+                        <td className={"border"}>
+                            <button onClick={()=>{deleteHandler(item.ID)}}>delete</button>
+                        </td>
                     </tr>
                     )
                 })
--- week 3/first-project/src/pages/index.tsx
+++ week 3/first-project/src/pages/index.tsx
@@ -11,15 +11,15 @@ export default function Home() {
   let product2=[
     {ID:4,name:"product 4",price:8200},
     {ID:5,name:"product 5",price:2200},
-    {ID:6,name:"product 6",price:1200}
+    {ID:6,name:"product 6",price:1200},
   ]
 
   return (
     <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-    <MyTable mydata={product}/>
-    <MyTable mydata={product2}/>
+    product && <MyTable mydata={product} caption={"product list 1"}/>
+    product2 &&<MyTable mydata={product2} caption={"product list 2"}/>
     </main>
   )
 }
