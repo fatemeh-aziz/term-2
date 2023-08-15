52693ebc424c5c3c9f7e431e8ed4285f4f18fcef
aLmA <fatemehazizi>
2023-08-15 16:29:52 +0330

    com

---
+++ week 4/weather-APP/weather-app/src/components/ForecastItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ForecastItem(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ForecastItem;
---
+++ week 4/weather-APP/weather-app/src/components/ForecatList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ForecatList(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ForecatList;
